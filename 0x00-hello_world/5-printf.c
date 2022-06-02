7fee308de0dfe4a263ef0c85c13fbf394903ac95
Felixgreat <felixnwaora2018@gmail.com>
2022-06-02 16:53:32 -0700

    C4

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,5 @@
 /**
  * main - Entry point
  *
- *  Return: Always 0 (Success)
- */
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+ *  Returl	return(0);
 }
