77069539191840ba426a668d4c0cc1107593e7bc
Felixgreat <105022777+Felixgreat@users.noreply.github.com>
2022-07-20 00:06:08 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,15 +2,15 @@
 
 /**
  * insert_nodeint_at_index - Inserts a new node to a listint_t
- * list at a given position.
- * @head: A pointer to the address of the
- * head of the listint_t list.
- * @idx: The index of the listint_t list where the new
- * node should be added - indices start at 0.
- * @n: The integer for the new node to contain.
+ *                           list at a given position.
+ * @head: Indicate a pointer to the address of the
+ *        head of the listint_t list.
+ * @idx: Indicate the index of the listint_t list where the new
+ *       node should be added - indices start at 0.
+ * @n: Indicate the integer for the new node to contain.
  *
  * Return: If the function fails - NULL.
- * Otherwise - the address of the new node.
+ *         Otherwise - the address of the new node.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new->n = n;
-	
+
 	if (idx == 0)
 	{
 		new->next = copy;
